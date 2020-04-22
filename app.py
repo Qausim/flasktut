eab59f9f28e4fa96cae40368cdf011c4d9564a47
Olawumi Qauzeem Yusuff <qauzeemyusuff2@gmail.com>
2020-04-22 09:39:16 +0100

    lession(authentication): implement user login

--- app.py
+++ app.py
@@ -1,14 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = 'my nigga'
 
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        print('logged_in' in session)
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
+# default method is get
 @app.route('/')
+@login_required
 def home():
-    return 'Hello, world'
+    return render_template('index.html')
+
 
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credential please try again'
+        else:
+            session['logged_in'] = True
+            flash('You just logged in!')
+            # redirect passing name of function to redirect to url_for
+            return redirect(url_for('home'))
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in')
+    flash('You just logged out!')
+    return redirect(url_for('welcome'))
 
 
 if __name__ == '__main__':
